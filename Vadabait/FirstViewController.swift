48c238f1d9a30bc0199a5c67392f0dacbd0106d9
Vladislav Dorfman <vladislav.dorfman@gmail.com>
2016-11-09 10:43:23 +0200

    qer
    qewr

---
+++ Vadabait/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Vadabait
+//
+//  Created by Vladislav Dorfman on 09/11/2016.
+//  Copyright © 2016 Vladislav Dorfman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
